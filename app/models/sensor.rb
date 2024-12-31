32f214a7f75ace46fe65cb15585d26f6af6a0434
Jef Amirault <jef@amirault.org>
2024-12-31 00:12:49 -0500

    format sensor updated at text

--- app/models/sensor.rb
+++ app/models/sensor.rb
@@ -10,4 +10,12 @@ class Sensor < ApplicationRecord
   def to_s
     self.name
   end
+
+  def last_temp
+    hygro_sensor_readings.last&.temp
+  end
+
+  def last_humidity
+    hygro_sensor_readings.last&.humidity
+  end
 end
