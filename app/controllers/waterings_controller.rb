d01026d18f685a020b2c3be27f14d7fcb42804b3
Jef Amirault <jef@amirault.org>
2025-03-16 10:30:54 -0400

    resolves #25; prevent duplicate waterings

--- app/controllers/waterings_controller.rb
+++ app/controllers/waterings_controller.rb
@@ -17,10 +17,10 @@ class WateringsController < ApplicationController
 
   # GET /waterings/new
   def new
-    @watering = Watering.create plant_id: params[:plant_id], date: Time.zone.now.to_date
+    @watering = Watering.new plant_id: params[:plant_id], date: Time.zone.now.to_date
     respond_to do |format|
       format.turbo_stream
-      format.html { redirect_to edit_watering_path(@watering) }
+      format.html
     end
   end
 
