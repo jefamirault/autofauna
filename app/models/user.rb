30e97da1d3c9db4a563a7ce60195d80d33d786b7
Jef Amirault <jef@amirault.org>
2025-08-17 18:42:45 -0400

    unique emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   validates :email, presence: true
   normalizes :email, with: -> { _1.strip.downcase }
+  validates :email, uniqueness: true
 
   generates_token_for :password_reset, expires_in: 15.minutes do
     password_salt&.last(10)
