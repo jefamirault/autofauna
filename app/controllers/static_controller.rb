af7c4d7c30bfb035459d26b6e3df58f7b4484469
Jef Amirault <jef@amirault.org>
2026-01-31 15:32:32 -0500

    static controller fingerprint

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,8 +5,8 @@ class StaticController < ApplicationController
       "relation" => ["delegate_permission/common.handle_all_urls"],
       "target" => {
         "namespace" => "android_app",
-        "package_name" => "com.autofauna.app",
-        "sha256_cert_fingerprints" => ["TODO:REPLACE:WITH:ACTUAL:FINGERPRINT"]
+        "package_name" => "org.autofauna.twa",
+        "sha256_cert_fingerprints" => ["19:78:DA:EF:25:40:95:D1:16:CF:31:20:24:FD:7D:00:67:02:A2:FF:63:91:DD:DB:73:D3:C9:F4:BD:6D:CC:8B"]
       }
     }]
   end
