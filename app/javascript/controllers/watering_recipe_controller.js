1c0a15c0f12c25501125fac38241934f5c657aba
Jef Amirault <jef@amirault.org>
2026-03-22 12:33:10 -0400

    Fix #88: Hide TDS field behind "Add TDS" button to declutter watering form
    TDS field is now hidden by default with an "Add TDS" button that reveals it.
    A remove button allows hiding it again (and clears the value). When editing
    a watering with existing TDS, the field shows automatically. Selecting a
    recipe batch with TDS also auto-reveals the field.

--- app/javascript/controllers/watering_recipe_controller.js
+++ app/javascript/controllers/watering_recipe_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["recipeSelect", "batchSelect", "tdsField"]
+  static targets = ["recipeSelect", "batchSelect", "tdsField", "tdsButton", "tdsContainer"]
   static values = { url: String }
 
   recipeChanged() {
@@ -27,6 +27,27 @@ export default class extends Controller {
     const selected = this.batchSelectTarget.selectedOptions[0]
     if (selected && selected.dataset.tds) {
       this.tdsFieldTarget.value = selected.dataset.tds
+      this.showTds()
+    }
+  }
+
+  toggleTds() {
+    this.tdsContainerTarget.style.display = "block"
+    this.tdsButtonTarget.style.display = "none"
+  }
+
+  hideTds() {
+    this.tdsContainerTarget.style.display = "none"
+    this.tdsButtonTarget.style.display = "inline-block"
+    this.tdsFieldTarget.value = ""
+  }
+
+  showTds() {
+    if (this.hasTdsContainerTarget) {
+      this.tdsContainerTarget.style.display = "block"
+    }
+    if (this.hasTdsButtonTarget) {
+      this.tdsButtonTarget.style.display = "none"
     }
   }
 }
