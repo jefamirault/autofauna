324331e68cedb6c328cacf70e124f63f2b34da7d
Jef Amirault <jef@amirault.org>
2026-03-22 12:33:49 -0400

    Fix #87: Show small plant graphic in header when minimized
    Add a small plant graphic thumbnail next to the title text in the
    collapsed header on plant/watering show/edit pages. The graphic
    fades in alongside the title when the header collapses via the
    existing --title-opacity CSS variable.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -86,12 +86,14 @@ module ApplicationHelper
     when 'plants', 'log_entries'
       section_title = t('layouts.application.plants')
       title = action.in?(%w[show edit]) && @plant ? @plant.to_s : section_title
-      { icon: 'autofauna_icon.png', title: title, section_title: section_title, gradient_class: 'header-plants', nav_path: plants_path, plant_graphic: nil }
+      graphic = action.in?(%w[show edit]) ? @plant&.graphic_path : nil
+      { icon: 'autofauna_icon.png', title: title, section_title: section_title, gradient_class: 'header-plants', nav_path: plants_path, plant_graphic: graphic }
     when 'waterings'
       section_title = t('layouts.application.waterings')
       plant = @watering&.plant || @plant
       title = action.in?(%w[show edit water quick_water]) && plant ? plant.to_s : section_title
-      { icon: 'water_icon.png', title: title, section_title: section_title, gradient_class: 'header-waterings', nav_path: waterings_path, plant_graphic: nil }
+      graphic = action.in?(%w[show edit new water quick_water]) ? plant&.graphic_path : nil
+      { icon: 'water_icon.png', title: title, section_title: section_title, gradient_class: 'header-waterings', nav_path: waterings_path, plant_graphic: graphic }
     when 'recipes'
       section_title = t('layouts.application.waterings')
       title = action.in?(%w[show edit]) && @recipe ? @recipe.to_s : section_title
